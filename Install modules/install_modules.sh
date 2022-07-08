feb6273387551d34613db18c44fd3a6cd91fa7f5
Yuri Ferreira <80469922+yobrisado@users.noreply.github.com>
2022-07-08 17:18:16 -0300

    Delete install.sh

--- Install modules/install_modules.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install wget -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-python -m pip install requests --quiet
-python -m pip install getpass3 --quiet
-
-python3 main.py
